289da46238ffaed0e1ddf8dc18987cc9971659bf
Clerton Leal <clerton.leal@wildlifestudios.com>
2021-06-19 09:17:03 -0300

    Add error messages and progress bar to loading pages

--- app/src/main/java/com/example/paymentlist/view/PaymentListActivity.java
+++ app/src/main/java/com/example/paymentlist/view/PaymentListActivity.java
@@ -1,10 +1,12 @@
 package com.example.paymentlist.view;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.paymentlist.R;
 import com.example.paymentlist.adapter.PaymentAdapter;
 import com.example.paymentlist.databinding.ActivityPaymentListBinding;
 import com.example.paymentlist.network.PaymentNetwork;
@@ -20,21 +22,34 @@ public class PaymentListActivity extends AppCompatActivity {
     @Inject
     PaymentNetwork service;
 
-    private ActivityPaymentListBinding activityMainBinding;
+    private ActivityPaymentListBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        activityMainBinding = ActivityPaymentListBinding.inflate(getLayoutInflater());
-        setContentView(activityMainBinding.getRoot());
+        binding = ActivityPaymentListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.toolbar.setNavigationIcon(R.drawable.arrow_left);
+        setSupportActionBar(binding.toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         PaymentListViewModel viewModel = new PaymentListViewModel(service);
 
         viewModel.getPaymentListObserver().observe(this,
-                listResult -> activityMainBinding.list.setAdapter(new PaymentAdapter(listResult.getNetworks().getApplicable()))
+                listResult -> binding.list.setAdapter(new PaymentAdapter(listResult.getNetworks().getApplicable()))
         );
+
         viewModel.getErrorMessageObserver().observe(this,
-                errorMessage -> Toast.makeText(PaymentListActivity.this, errorMessage, Toast.LENGTH_LONG).show()
+                errorMessage -> {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage(errorMessage);
+                    builder.setPositiveButton(R.string.retry, (dialog, id) -> viewModel.loadPayments());
+                    builder.create().show();
+                }
+        );
+
+        viewModel.getProgressObserver().observe(this,
+                aBoolean -> binding.progressBar.setVisibility(aBoolean ? View.VISIBLE : View.GONE)
         );
 
         getLifecycle().addObserver(viewModel);
--- app/src/main/java/com/example/paymentlist/viewmodel/PaymentListViewModel.java
+++ app/src/main/java/com/example/paymentlist/viewmodel/PaymentListViewModel.java
@@ -2,6 +2,7 @@ package com.example.paymentlist.viewmodel;
 
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.OnLifecycleEvent;
 import androidx.lifecycle.ViewModel;
@@ -9,11 +10,14 @@ import androidx.lifecycle.ViewModel;
 import com.example.paymentlist.network.PaymentNetwork;
 import com.example.paymentlist.model.ListResult;
 
+import org.jetbrains.annotations.NotNull;
+
 import javax.inject.Inject;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import timber.log.Timber;
 
 public class PaymentListViewModel extends ViewModel implements LifecycleObserver {
 
@@ -21,6 +25,7 @@ public class PaymentListViewModel extends ViewModel implements LifecycleObserver
 
     private final MutableLiveData<ListResult> paymentList = new MutableLiveData<>();
     private final MutableLiveData<String> errorMessage = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> progress = new MutableLiveData<>();
 
     @Inject
     public PaymentListViewModel(PaymentNetwork paymentNetwork) {
@@ -28,20 +33,27 @@ public class PaymentListViewModel extends ViewModel implements LifecycleObserver
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-    private void onCreate() {
+    public void loadPayments() {
+        progress.postValue(true);
         paymentNetwork.getPayments().enqueue(new Callback<ListResult>() {
             @Override
-            public void onResponse(Call<ListResult> call, Response<ListResult> response) {
+            public void onResponse(@NotNull Call<ListResult> call, @NotNull Response<ListResult> response) {
                 if (response.isSuccessful()) {
                     paymentList.postValue(response.body());
+                    errorMessage.postValue("We can't access the page. =/ \nPlease verify you internet connection");
                 } else {
-                    errorMessage.postValue("Deu ruim, status code:" + response.code());
+                    errorMessage.postValue("We have a internal error: " + response.code());
+                    Timber.e("Error to request payments: %s", response.toString());
                 }
+
+                progress.postValue(false);
             }
 
             @Override
-            public void onFailure(Call<ListResult> call, Throwable t) {
-                errorMessage.postValue("Deu ruim: " + t);
+            public void onFailure(@NotNull Call<ListResult> call, @NotNull Throwable t) {
+                errorMessage.postValue("We can't access the page. =/ \nPlease verify you internet connection");
+                progress.postValue(false);
+                Timber.e(t, "Internal error to call payments");
             }
         });
     }
@@ -51,11 +63,15 @@ public class PaymentListViewModel extends ViewModel implements LifecycleObserver
         paymentNetwork.getPayments().cancel();
     }
 
-    public MutableLiveData<ListResult> getPaymentListObserver() {
+    public LiveData<ListResult> getPaymentListObserver() {
         return paymentList;
     }
 
-    public MutableLiveData<String> getErrorMessageObserver() {
+    public LiveData<String> getErrorMessageObserver() {
         return errorMessage;
+    }
+
+    public LiveData<Boolean> getProgressObserver() {
+        return progress;
     }
 }
